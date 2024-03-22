3173583d68957a1f1e308f8bce6ea0f99214fc2d
Cristian Anjari <93675228+SnakeJuice@users.noreply.github.com>
2024-03-22 18:03:17 -0300

    Updated documentation 22/03

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # - Cristian Anjari                                      #
 # - Marcos Medina                                        #
 #                                                        #
-# Para proyecto de Tesis 2023                            #
+# Para proyecto de Tesis 2024                            #
 #                                                        #
 # Universidad de Santiago de Chile                       #
 # Facultad de Ciencia                                    #
@@ -15,7 +15,7 @@
 # Analista en Computación Científica                     #
 #                                                        #
 # Santiago, Chile                                        #
-# 03/01/2024                                             #
+# 25/03/2024                                             #
 ##########################################################
 
 from pybricks.hubs import EV3Brick
@@ -25,7 +25,6 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from pybricks.messaging import BluetoothMailboxClient, TextMailbox
-
 import math
 
 # Inicializa el ladrillo EV3 y los motores
@@ -41,7 +40,9 @@ gyro = GyroSensor(Port.S3)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=125)
 robot.settings(30,20,50,20)
-##########################################
+
+#################################################
+
 ROBOT_SPEED = 30 #Velocidad al avanzar
 turn_speed_axis = 30 #Velocidad de giro en eje
 turn_speed_turn = 45 #Velocidad de giro en curva
@@ -58,7 +59,14 @@ cuad = 0
 red = []
 green = []
 
-# Gyro straight
+"""
+    Esta función permite al robot avanzar en línea recta utilizando el sensor giroscópico para mantener la dirección.
+    Durante el avance, el robot detecta objetos y guarda su coordenada discriminando si son rojos o verdes.
+
+    Parámetros:
+    distance (int): La distancia que el robot debe recorrer.
+    is_second_run (bool): Un indicador de si el robot está en una vuelta par del recorrido.
+"""
 def gyro_straight(distance, is_second_run=False):
     gyro.reset_angle(0)
     robot.reset() # Reset robot
@@ -66,46 +74,52 @@ def gyro_straight(distance, is_second_run=False):
     red_detected = False
     green_detected = False
 
+    # Mientras la distancia recorrida por el robot sea menor o igual a la distancia objetivo
     while robot.distance() <= distance:
         correction = (0 - gyro.angle())*3 # Calcula la correccion
         robot.drive(ROBOT_SPEED, correction) # Conduce el robot
 
-        #Detecta Rojo!
+        # Si el sensor de color detecta un objeto rojo
         if color_sensor.color() == Color.RED and not red_detected:
             red_count = red_count + 1
             ev3.screen.print("Red:",red_count)
-            red_detected = True
+            red_detected = True # Marca la flag de objeto rojo detectado
             #print("Estado red:", red_detected)
 
+            # Si es una vuelta par en el recorrido
             if is_second_run:
                 print("SEGUNDA VUELTA!")
-                distancia_actual = object_distance()
-                distancia_restante = distance - distancia_actual
-                distancia_restante = distancia_restante - 17
+                distancia_actual = object_distance() # Obtenemos la distancia actual recorrida por el robot
+                distancia_restante = distance - distancia_actual # Calculamos la distancia restante
+                distancia_restante = distancia_restante - 17 # Y le restamos el tamaño del robot
                 #print("dist_rest", distancia_restante)
-                object_coords(distancia_restante,Red=True)
+                object_coords(distancia_restante,Red=True) # Guardamos las coordenadas del objeto rojo
+            # Si es una vuelta impar
             else:
-                distancia_actual = object_distance()
-                object_coords(distancia_actual,Red=True)
+                distancia_actual = object_distance() # Obtenemos la distancia actual recorrida por el robot
+                object_coords(distancia_actual,Red=True) # Guardamos las coordenadas del objeto rojo
         
-        #Detecta Verde!
+        # Si el sensor de color detecta un objeto verde
         if color_sensor.color() == Color.GREEN and not green_detected:
             green_count = green_count + 1
             ev3.screen.print("Green:",green_count)
-            green_detected = True
+            green_detected = True # Marca la flag de objeto verde detectado
             #print("Estado green:", green_detected)
 
+            # Si es una vuelta par en el recorrido
             if is_second_run:
                 print("SEGUNDA VUELTA!")
-                distancia_actual = object_distance()
-                distancia_restante = distance - distancia_actual
-                distancia_restante = distancia_restante - 17
+                distancia_actual = object_distance() # Obtenemos la distancia actual recorrida por el robot
+                distancia_restante = distance - distancia_actual # Calculamos la distancia restante
+                distancia_restante = distancia_restante - 17 # Y le restamos el tamaño del robot
                 #print("dist_rest", distancia_restante)
-                object_coords(distancia_restante,Green=True)
+                object_coords(distancia_restante,Green=True) # Guardamos las coordenadas del objeto verde
+            # Si es una vuelta impar
             else:
-                distancia_actual = object_distance()
-                object_coords(distancia_actual,Green=True)
+                distancia_actual = object_distance() # Obtenemos la distancia actual recorrida por el robot
+                object_coords(distancia_actual,Green=True) # Guardamos las coordenadas del objeto verde
 
+        # Si deja de detectar un objeto rojo o verde, reinicia las flags
         elif color_sensor.color() == Color.BLUE or color_sensor.color() == Color.WHITE:
             red_detected = False
             green_detected = False
@@ -113,41 +127,58 @@ def gyro_straight(distance, is_second_run=False):
     robot.stop()
     left_motor.brake()
     right_motor.brake()
-    
+#################################################
+
+"""
+    Esta función ayuda a calcular la distancia total recorrida por el robot, utilizando el promedio de las vueltas 
+    de los motores y la circunferencia de las ruedas.
+
+    Devuelve:
+    float: La distancia total recorrida por el robot.
+"""
 def object_distance():
-    # Calcular la distancia total recorrida por el robot
     return (left_motor.angle() + right_motor.angle()) * 0.5 * (circunferencia_rueda / 360.0)
+#################################################
     
-    
+"""
+    Esta función guarda las coordenadas de los objetos detectados.
+
+    Parámetros:
+    distancia (int): La distancia al objeto detectado.
+    Red (bool): Un indicador de si el objeto detectado es rojo.
+    Green (bool): Un indicador de si el objeto detectado es verde.
+"""
 def object_coords(distancia,Red=False,Green=False):
     global red,green
 
-    distancia = math.floor(distancia/10)
+    distancia = math.floor(distancia/10) # Se transforma la distancia a centímetros
     
     # Si se detecta un objeto rojo
     if Red == True:
-        distancia = distancia + 10
+        #distancia = distancia + 10
         coordenada = (distancia,cuad)
         red.append(coordenada)
         print("Red:",red)
 
     # Si se detecta un objeto verde    
     if Green == True:
-        distancia = distancia
+        #distancia = distancia
         coordenada = (distancia,cuad)
         green.append(coordenada)
         print("Green:",green)
+#################################################
 
 
-############# GIROS EN EJE #############
+################ GIROS EN EJE  ##################
 # DERECHA
 def gyro_right_axis(degrees):
     gyro.reset_angle(0)
-    target_angle = gyro.angle() + degrees
+    target_angle = gyro.angle() + degrees # Calcula el angulo objetivo
 
-    while gyro.angle() < target_angle : # Si el gyro es menor que los angulo objetivo
-        left_motor.run(turn_speed_axis) # Gira a la derecha
-        right_motor.run(-turn_speed_axis)
+    # Mientras el gyro sea menor que el angulo objetivo
+    while gyro.angle() < target_angle :
+        left_motor.run(turn_speed_axis) # El motor izquierdo gira hacia la derecha
+        right_motor.run(-turn_speed_axis) # El motor derecho gira hacia la izquierda
 
     left_motor.brake()
     right_motor.brake()
@@ -155,42 +186,46 @@ def gyro_right_axis(degrees):
 # IZQUIERDA
 def gyro_left_axis(degrees):
     gyro.reset_angle(0)
-    target_angle = gyro.angle() - degrees
+    target_angle = gyro.angle() - degrees # Calcula el angulo objetivo
 
-    while gyro.angle() > target_angle : # Si el gyro es mayor que el angulo objetivo
-        right_motor.run(turn_speed_axis) # Gira a la izquierda
-        left_motor.run(-turn_speed_axis)
+    # Mientras el gyro sea mayor que el angulo objetivo
+    while gyro.angle() > target_angle :
+        right_motor.run(turn_speed_axis) # El motor derecho gira hacia la derecha
+        left_motor.run(-turn_speed_axis) # El motor izquierdo gira hacia la izquierda
 
     left_motor.brake()
     right_motor.brake()
-##########################################
+#################################################
 
 
 
-############# GIROS EN CURVA #############
+############### GIROS EN CURVA  #################
 # DERECHA
 def gyro_right_turn(degrees):
     gyro.reset_angle(0)
-    target_angle = gyro.angle() + degrees
+    target_angle = gyro.angle() + degrees # Calcula el angulo objetivo
 
-    while gyro.angle() < target_angle : # Si el gyro es menor que los angulo objetivo
-        left_motor.run(turn_speed_turn) # Gira a la derecha
+    # Mientras el gyro sea menor que el angulo objetivo
+    while gyro.angle() < target_angle :
+        left_motor.run(turn_speed_turn) # El motor izquierdo gira hacia la derecha
 
     left_motor.brake()
 
 # IZQUIERDA
 def gyro_left_turn(degrees):
     gyro.reset_angle(0)
-    target_angle = gyro.angle() - degrees
+    target_angle = gyro.angle() - degrees # Calcula el angulo objetivo
 
-    while gyro.angle() > target_angle : # Si el gyro es mayor que el angulo objetivo
-        right_motor.run(turn_speed_turn) # Gira a la izquierda
+    # Mientras el gyro sea mayor que el angulo objetivo
+    while gyro.angle() > target_angle :
+        right_motor.run(turn_speed_turn) # El motor derecho gira hacia la derecha
 
     right_motor.brake()
 #################################################
 
+
 #################################################
-##----------------Partida----------------------##
+##----------------- main ----------------------##
 #################################################
 
 SERVER = 'Master'
@@ -210,7 +245,7 @@ while True:
     if(mbox.read()=='inicia buscador'):
 
         #primera vuelta
-        cuad = 17
+        cuad = cuad + 17
         gyro_straight(rec_dist)
         gyro_left_axis(90)
         gyro_straight(170)
@@ -248,7 +283,7 @@ while True:
 
         #cuarta vuelta
         cuad= cuad + 17
-        gyro_straight(rec_dist)
+        gyro_straight(rec_dist,is_second_run=True)
         gyro_right_axis(90)
         gyro_straight(170)
         gyro_right_axis(90)
@@ -272,7 +307,7 @@ while True:
 
         #sexta
         cuad= cuad + 17
-        gyro_straight(rec_dist)
+        gyro_straight(rec_dist,is_second_run=True)
         gyro_right_axis(90)
         gyro_straight(170)
         gyro_right_axis(90)
